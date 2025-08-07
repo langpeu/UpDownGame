5b79619ea91a5737948a274cfabddfc133dddd32
langpeu <icopy@naver.com>
2025-08-07 13:06:11 +0900

    - selNum 없이 label 에서 직접 선택한 숫자 가져와서 처리하기

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var numberLabel: UILabel!
     
     var comNum = Int.random(in: 1...10)
-    var selNum = -1 // 비선택시 예외처리를 위해 -1 저장
+    //var selNum = -1 // 비선택시 예외처리를 위해 -1 저장
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,26 +28,25 @@ class ViewController: UIViewController {
         // 1) 버튼의 숫자를 가져와야함
         // 2) 숫자 레이블이 변하도록 (숫자에 따라)
         // 3) 선택한 숫자를 변수에다가 저장 (선택)
-        if let btnLabel = sender.currentTitle,
-           let btnNum = Int(btnLabel) {
-            selNum = btnNum
-            numberLabel.text = btnLabel
-        }
-        
+        guard let numString = sender.currentTitle else { return }
+        numberLabel.text = numString
+
     }
     
     
     @IBAction func selectButtonTapped(_ sender: UIButton) {
         // 1) 컴퓨터의 숫자와 내가 선택한 숫자를 비교 UP / Down / Bingo (메인레이블)
-        if selNum != -1 {
-            if comNum == selNum {
-                mainLabel.text = "Bingo😍"
-            }else if comNum > selNum {
-                mainLabel.text = "UP"
-            }else if comNum < selNum {
-                mainLabel.text = "DOWN"
-            }
+        guard let selNumString = numberLabel.text else { return }
+        guard let selNum = Int(selNumString) else { return }
+    
+        if comNum == selNum {
+            mainLabel.text = "Bingo 😍"
+        }else if comNum > selNum {
+            mainLabel.text = "UP"
+        }else if comNum < selNum {
+            mainLabel.text = "DOWN"
         }
+    
     }
     
     
@@ -57,7 +56,6 @@ class ViewController: UIViewController {
     
     func resetGame() {
         comNum = Int.random(in: 1...10)
-        selNum = -1
         mainLabel.text = "선택하세요"
         numberLabel.text = ""
     }
